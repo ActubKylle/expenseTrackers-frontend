aaffa3fbdbe5b252bfa78ea919ee2968ea67dbae
KylleActub <occ.actub.kyllenichole@gmail.com>
2025-05-08 18:53:33 +0800

    5/8/25

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://expense-tracker.test/api'; 
+const API_URL = 'https://expense.dftools.site/api'; 
 const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
